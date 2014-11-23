cbd21fbf732989a138a03b9f1553bb1077651fff
leopoldkwok <leopoldkwok@gmail.com>
2014-11-23 23:51:27 +0000

    tests now passing

--- lib/student.rb
+++ lib/student.rb
@@ -2,22 +2,24 @@ class Student
 
 	 # attr_reader :student
 	
-	def initialize(student)
+	def initialize
 	 	 	@badges = []
 	  end
 
 	def award(badge)
 		@badges << badge
-		self.class.send(:define_method, "has_#{badge}?"){return true}
+		# self.class.send(:define_method, "has_#{badge}?"){return true}
 	end
 
 	 
 
  	def method_missing(method_name)
-    	if method_name =~ /^has_/+?\?/
-    		return false
-    		else
+    	if method_name =~ /^has_(.+?)\?/
+    		self.class.send(:define_method, method_name) {@badges.include?($1.to_sym) ? true : false}
+    		self.send(method_name)
+    
+    	else
     			super
-    		end
     	end
+end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -5,12 +5,17 @@ describe Student do
     let(:student){Student.new}
 
     it "does not have an award" do 
-        #expect(student.has_unixoid?).to eq(false)
-        expect(student).not_to have_unixoid
+        expect(student.has_unixoid?).to eq(false)
+        # expect(student).not_to have_unixoid
     end
 
      it "can have badges awarded" do 
          student.award :unixoid
          expect(student).to have_unixoid
+     end
+
+     it "can create an attribute reader" do 
+     	student.award :unixoid
+     	expect(student.class.instance_methods(false)).to include(:has_unixoid?)
      end
 end
